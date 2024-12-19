23ebc3753d86075c5ee7a4aee3133de73a615c42
Fxker-C <bcontreras0501@outlook.com>
2024-12-19 18:45:31 -0500

    Initial Commit

---
+++ BasicCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicCalculator
+//
+//  Created by Aly Benjamin Contreras Del Pino on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Benjamin :D!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
