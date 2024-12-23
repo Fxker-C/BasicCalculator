9e64b6fa49b4e505aceb4d81c100c410c8fe8b77
Fxker-C <bcontreras0501@outlook.com>
2024-12-23 14:01:07 -0500

    Creación de función clear

--- BasicCalculator/ContentView.swift
+++ BasicCalculator/ContentView.swift
@@ -9,18 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var currentNumber = 0
+    @State private var currentNumber = "0"
+    @State private var previousNumber = "0"
+    @State private var operation:String? = nil
     
     var body: some View {
         VStack {
             HStack{
-                Button("1"){numberPressed("7")}
+                Button("1"){numberPressed("1")}
+                Button("2"){numberPressed("2")}
             }
         }
     }
     
     func numberPressed(_ number:String){
-        
+        if currentNumber == "0" {
+            currentNumber = number
+        }else{
+            currentNumber += number
+        }
+    }
+    
+    func clear(){
+        currentNumber = "0"
+        previousNumber = "0"
+        operation = nil
     }
 }
 
