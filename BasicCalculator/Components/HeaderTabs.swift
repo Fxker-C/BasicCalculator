0b566b21b83881302c205be049ffa567ee4cf6dd
Benjamin Contreras <benjacontreras.6000@gmail.com>
2025-02-07 17:50:06 -0500

    add HeaderTabs

--- BasicCalculator/Components/HeaderTabs.swift
+++ BasicCalculator/Components/HeaderTabs.swift
@@ -4,5 +4,31 @@
 //
 //  Created by Aly Benjamin Contreras Del Pino on 7/02/25.
 //
+import SwiftUI
 
-import Foundation
+struct HeaderTabs: View {
+    @Binding var selectedView: SelectedView
+
+    var body: some View {
+        HStack(spacing: 20) {
+            TabButton(title: "Calculadora", view: .calculator, selectedView: $selectedView)
+            TabButton(title: "Convertidor", view: .converter, selectedView: $selectedView)
+            TabButton(title: "Historial", view: .history, selectedView: $selectedView)
+        }
+        .padding()
+    }
+}
+
+struct TabButton: View {
+    let title: String
+    let view: SelectedView
+    @Binding var selectedView: SelectedView
+
+    var body: some View {
+        Button(action: { selectedView = view }) {
+            Text(title)
+                .foregroundColor(selectedView == view ? .blue : .black)
+                .fontWeight(selectedView == view ? .bold : .regular)
+        }
+    }
+}
