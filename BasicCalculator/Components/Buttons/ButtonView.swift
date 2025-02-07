25b384b5f7d81371c5c8bc92dd92d94b2534c78f
Benjamin Contreras <benjacontreras.6000@gmail.com>
2025-02-07 17:49:47 -0500

    add Buttonview

--- BasicCalculator/Components/Buttons/ButtonView.swift
+++ BasicCalculator/Components/Buttons/ButtonView.swift
@@ -4,5 +4,32 @@
 //
 //  Created by Aly Benjamin Contreras Del Pino on 7/02/25.
 //
+import SwiftUI
 
-import Foundation
+struct ButtonView: View {
+    let title: String
+    let backgroundColor: Color
+    let action: () -> Void
+
+    init(title: String, backgroundColor: Color = .gray, action: @escaping () -> Void) {
+        self.title = title
+        self.backgroundColor = backgroundColor
+        self.action = action
+    }
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .frame(width: 70, height: 70)
+                .background(backgroundColor)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+    }
+}
+
+#Preview {
+    ButtonView(title: "7") { print("Pressed 7") }
+}
+
