da97e9d62882bdef398842823e1703d0199416ba
Benjamin Contreras <benjacontreras.6000@gmail.com>
2025-02-11 01:12:34 -0500

    add ConversionCategory

--- BasicCalculator/Models/ConversionCategory.swift
+++ BasicCalculator/Models/ConversionCategory.swift
@@ -4,5 +4,38 @@
 //
 //  Created by Aly Benjamin Contreras Del Pino on 11/02/25.
 //
-
 import Foundation
+
+enum ConversionCategory: String, CaseIterable {
+    case divisas = "Divisas"
+    case longitud = "Longitud"
+    case masa = "Masa"
+    case area = "Área"
+    case tiempo = "Tiempo"
+    case datos = "Datos"
+    case descuento = "Descuento"
+    case volumen = "Volumen"
+    case sistemaNumerico = "Sistema numérico"
+    case velocidad = "Velocidad"
+    case temperatura = "Temperatura"
+    case imc = "IMC"
+    
+    var systemImage: String {
+        switch self {
+        case .divisas: return "arrow.2.circlepath"
+        case .longitud: return "text.alignleft"
+        case .masa: return "scalemass"
+        case .area: return "square"
+        case .tiempo: return "clock"
+        case .datos: return "list.bullet.rectangle"
+        case .descuento: return "tag"
+        case .volumen: return "cube"
+        case .sistemaNumerico: return "number"
+        case .velocidad: return "gauge"
+        case .temperatura: return "thermometer"
+        case .imc: return "person"
+        }
+    }
+}
+
+
