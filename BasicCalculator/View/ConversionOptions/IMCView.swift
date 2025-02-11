0d322780b37d307b42e159fad763e46f7f40bb94
Benjamin Contreras <benjacontreras.6000@gmail.com>
2025-02-11 01:19:12 -0500

    u

--- BasicCalculator/View/ConversionOptions/IMCView.swift
+++ BasicCalculator/View/ConversionOptions/IMCView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct IMCView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("IMC")
+            .font(.title)
     }
 }
 
--- BasicCalculator/View/ConversionOptions/MassView.swift
+++ BasicCalculator/View/ConversionOptions/MassView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct MassView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Masa")
+            .font(.title)
     }
 }
 
