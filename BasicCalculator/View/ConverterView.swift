63beb65dc6df1e06eff69ff294c22d87aab37bb8
Benjamin Contreras <benjacontreras.6000@gmail.com>
2025-02-11 01:13:41 -0500

    u

--- BasicCalculator/View/ConverterView.swift
+++ BasicCalculator/View/ConverterView.swift
@@ -8,7 +8,42 @@
 import SwiftUI
 
 struct ConverterView: View {
+    @State private var selectedCategory: ConversionCategory?
+
+    let columns = [
+        GridItem(.flexible(), spacing: 20),
+        GridItem(.flexible(), spacing: 20),
+        GridItem(.flexible(), spacing: 20)
+    ]
+
     var body: some View {
-        Text("Convertidor :D")
+        NavigationStack {
+            VStack {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(ConversionCategory.allCases, id: \.self) { category in
+                        CategoryButton(category: category) {
+                            selectedCategory = category
+                        }
+                    }
+                }
+                .padding()
+            }
+            .navigationDestination(item: $selectedCategory) { category in
+                switch category {
+                case .divisas: ForeignExchangeView()
+                case .longitud: LengthView()
+                case .masa: MassView()
+                case .area: AreaView()
+                case .tiempo: TimeView()
+                case .datos: DataView()
+                case .descuento: DiscountView()
+                case .volumen: VolumeView()
+                case .sistemaNumerico: NumberSystemView()
+                case .velocidad: SpeedView()
+                case .temperatura: TemperatureView()
+                case .imc: IMCView()
+                }
+            }
+        }
     }
 }
